4776891fdcc891824dcfab798c8a509e4f255560
Romain PROVIN <rprovin@iliad.fr>
2022-12-08 11:56:29 +0100

    first project setup

---
+++ src/main/java/com/team1617/stock11/Application.java
@@ -0,0 +1,13 @@
+package com.team1617.stock11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
